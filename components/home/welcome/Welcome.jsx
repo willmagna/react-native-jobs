fa9e74aa76c85bcd656409c991d9da21f50ba348
William Magnabosco <williamlodea@gmail.com>
2023-12-09 18:54:25 -0400

    a

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,22 +1,23 @@
-import { useState } from 'react';
-import { 
-  View, 
+import { useState } from "react";
+import {
+  View,
   Text,
   TextInput,
   TouchableOpacity,
   Image,
-  FlatList
-} from 'react-native';
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
 
-import { useRouter } from 'expo-router';
-import styles from './welcome.style';
-
-import { icons, SIZES } from '../../../constants';
+import styles from "./welcome.style";
+import { icons, SIZES } from "../../../constants";
 
 const jobTypes = ["Full-time", "Part-time", "Contractor"];
 
-const Welcome = () => {
+const Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
   const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState("Full-time");
+
   return (
     <View>
       <View style={styles.container}>
@@ -28,36 +29,42 @@ const Welcome = () => {
         <View style={styles.searchWrapper}>
           <TextInput
             style={styles.searchInput}
-            value=""
-            onChange={() => {}}
-            placeholder="What Are you looking for?"
+            value={searchTerm}
+            onChangeText={(text) => setSearchTerm(text)}
+            placeholder='What are you looking for?'
           />
         </View>
 
-        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+        <TouchableOpacity style={styles.searchBtn} onPress={handleClick}>
           <Image
             source={icons.search}
-            resizeMode="contain"
+            resizeMode='contain'
             style={styles.searchBtnImage}
           />
         </TouchableOpacity>
-
       </View>
 
       <View style={styles.tabsContainer}>
-        <FlatList 
-          //scrollEnabled={false}
+        <FlatList
           data={jobTypes}
-          renderItem={ ({ item }) => {
-            <TouchableOpacity>
-              <Text>{item}</Text>
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`);
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
             </TouchableOpacity>
-          }}
+          )}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
         />
       </View>
-
     </View>
-  )
-}
+  );
+};
 
 export default Welcome;
--- components/home/welcome/welcome.style.js
+++ components/home/welcome/welcome.style.js
@@ -7,12 +7,12 @@ const styles = StyleSheet.create({
     width: "100%",
   },
   userName: {
-    //fontFamily: FONT.regular,
+    fontFamily: FONT.regular,
     fontSize: SIZES.large,
     color: COLORS.secondary,
   },
   welcomeMessage: {
-    //fontFamily: FONT.bold,
+    fontFamily: FONT.bold,
     fontSize: SIZES.xLarge,
     color: COLORS.primary,
     marginTop: 2,
@@ -34,7 +34,7 @@ const styles = StyleSheet.create({
     height: "100%",
   },
   searchInput: {
-    //fontFamily: FONT.regular,
+    fontFamily: FONT.regular,
     width: "100%",
     height: "100%",
     paddingHorizontal: SIZES.medium,
@@ -64,7 +64,7 @@ const styles = StyleSheet.create({
     borderColor: activeJobType === item ? COLORS.secondary : COLORS.gray2,
   }),
   tabText: (activeJobType, item) => ({
-    //fontFamily: FONT.medium,
+    fontFamily: FONT.medium,
     color: activeJobType === item ? COLORS.secondary : COLORS.gray2,
   }),
 });
